f0e6d9f241e7a687c18d294de81762c67f433f53
rcfgama <rcfgama@gmail.com>
2020-06-13 09:50:28 -0300

    Gerenciador de Tarefas
    Projeto de um site/aplicativo em angular de um Gerenciador de tarefas. Projeto feito a partir do curso angular.

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar-tarefa/cadastrar-tarefa.component';
+
+@NgModule({
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent
+  ],
+  providers: [
+    TarefaService
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ]
+})
+export class TarefasModule { }
